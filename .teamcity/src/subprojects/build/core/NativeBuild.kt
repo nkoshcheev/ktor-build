06ee55f9746bd85d58e4c21ae44b6465fb8536d8
Hadi Hariri <mail@hadihariri.com>
2020-10-27 12:45:36 +0100

    KTOR-1114 Refactor entries and extract as variables

--- .teamcity/src/subprojects/build/core/NativeBuild.kt
+++ .teamcity/src/subprojects/build/core/NativeBuild.kt
@@ -9,7 +9,7 @@ import subprojects.build.*
 class NativeBuild(private val osEntry: OSEntry) : BuildType({
     id("KtorMatrixNative_${osEntry.name}".toExtId())
     name = "Native on ${osEntry.name}"
-    artifactRules = addArtifacts("+:**/build/**/*.klib", junitReportArtifact, memoryReportArtifact)
+    artifactRules = formatArtifactsString("+:**/build/**/*.klib", junitReportArtifact, memoryReportArtifact)
     vcs {
         root(VCSCore)
     }
